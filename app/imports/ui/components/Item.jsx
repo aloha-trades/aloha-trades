449bf9468a651f910daf75c05a7ac4e80ac1553a
gahernan14 <143157163+gahernan14@users.noreply.github.com>
2023-11-26 07:15:06 -1000

    ESLint Error Correction
    Fixed ESLint errors that were causing errors on github running the application

--- app/imports/ui/components/Item.jsx
+++ app/imports/ui/components/Item.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Card,} from "react-bootstrap";
+import { Card } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Item = ({ item }) => (
-    <Card className="h-100">
-        <Card.Header>
-            <Card.Title>SellerName: {item.sellerName}</Card.Title>
-            <Card.Subtitle>ItemName:{item.itemName}  ItemPrice:{item.price}</Card.Subtitle>
-        </Card.Header>
-        <Card.Body>
-            {item.description}
-        </Card.Body>
-    </Card>
+  <Card className="h-100">
+    <Card.Header>
+      <Card.Title>SellerName: {item.sellerName}</Card.Title>
+      <Card.Subtitle>ItemName:{item.itemName}  ItemPrice:{item.price}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      {item.description}
+    </Card.Body>
+  </Card>
 );
 
 // Require a document to be passed to this component.
 Item.propTypes = {
-    item: PropTypes.shape({
-        sellerName: PropTypes.string,
-        itemName: PropTypes.string,
-        price: PropTypes.number,
-        description: PropTypes.string,
-        // _id: PropTypes.string,
-    }).isRequired,
+  item: PropTypes.shape({
+    sellerName: PropTypes.string,
+    itemName: PropTypes.string,
+    price: PropTypes.number,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
 };
 
 export default Item;
